afc0894e614e5dc9eea8da3022e5ae2f9d4d6d4c
AlecCross <cross2472@gmail.com>
2022-01-13 18:56:06 +0200

    Проверка пользователя по токену и доступ к dashboard

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(cookieParser())
 
-
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -34,14 +33,58 @@ app.post('/login', (req, res) => {
             // Or, if multiple clients access the backend:
             //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
         });
-        const payload = ticket.getPayload();
-        const userid = payload['sub'];
+        const payload = ticket.getPayload()
+        const userid = payload['sub']
         // If request specified a G Suite domain:
         // const domain = payload['hd'];
         console.log(payload)
     }
-    verify().catch(console.error);
+    verify().catch(console.error)
+        .then(() => {
+            res.cookie('session-token', token)
+            res.send('success')
+        }).catch(console.error)
 })
+
+app.get('/dashboard', checkAuthenticated, (req, res) => {
+    let user = req.user
+    res.render('dashboard', { user })
+})
+
+app.get('/protectedroute', (req, res) => {
+    res.render('protectedroute.ejs')
+})
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('session-token')
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+
+    let token = req.cookies['session-token'];
+
+    let user = {}
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload()
+        user.name = payload.name
+        user.email = payload.email
+        user.picture = payload.picture;
+    }
+    verify()
+        .then(() => {
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            res.redirect('/login')
+        })
+
+}
 
 app.listen(PORT, () => {
     console.log('Server running on port: ' + PORT)
